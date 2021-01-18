4a4d7fed24bfc103cbdc84480896435075972745
Baldaz02 <51002697+Baldaz02@users.noreply.github.com>
2021-01-18 16:23:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(function (req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+})
+
+app.get('/', (req,res) =>{
+    res.send('Hello World, siamo nella radice')
+})
+
+app.get('/f1', (req,res) =>{
+    res.send('Hello World, siamo in F1')
+})
+
+
+app.listen(port, () =>{
+    console.log('Example app listening at http://localhost:3000')
+})
